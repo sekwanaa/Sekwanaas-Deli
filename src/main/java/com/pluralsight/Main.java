8740eca10d2033be32b957dd4168ca8d1180afbe
sekwanaa <sk123rulz@gmail.com>
2024-05-02 13:49:55 -0400

    Added some structure to creating an order

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,39 @@
 package com.pluralsight;
 
-public class Main {
-    public static void main(String[] args) {
+import com.pluralsight.userInterfaces.Order;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nWelcome to our DELI-cious application");
+                System.out.println("What can we do for you today?");
+                System.out.println("""
+                
+                [1] Create a new order
+                [0] Exit
+                """);
+                System.out.print("Enter choice: ");
+                int userChoice = scanner.nextInt();
+
+                switch (userChoice) {
+                    case 1 -> {
+                        Order userOrder = new Order();
+                        userOrder.homeScreen(scanner);
+                    }
+                    case 0 -> System.exit(0);
+                    default -> System.out.println("That's not a valid choice, please try again...");
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Wrong input type, please try again...");
+        }
     }
+
+
 }
--- src/main/java/com/pluralsight/userInterfaces/Order.java
+++ src/main/java/com/pluralsight/userInterfaces/Order.java
@@ -1,4 +1,55 @@
 package com.pluralsight.userInterfaces;
 
+import com.pluralsight.models.Chips;
+import com.pluralsight.models.Drinks;
+import com.pluralsight.models.Sandwich;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Order {
+    private List<Sandwich> sandwich;
+    private List<Drinks> drinks;
+    private List<Chips> chips;
+
+    public void homeScreen(Scanner scanner) {
+        while (true) {
+            System.out.println("""
+                What would you like to add to your order?
+                
+                [1] Sandwich
+                [2] Drinks
+                [3] Chips
+                [4] Sides
+                [5] Sauces
+                [0] Previous screen
+                """);
+            int userChoice = scanner.nextInt();
+
+            switch (userChoice) {
+                case 1:
+                    System.out.println("Sandwiches");
+                    break;
+                case 2:
+                    System.out.println("Drinks");
+                    break;
+                case 3:
+                    System.out.println("Chips");
+                    break;
+                case 4:
+                    System.out.println("Sides");
+                    break;
+                case 5:
+                    System.out.println("Sauces");
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("That's not a valid choice, please try again...");
+            }
+        }
+    }
+
+
+
 }
