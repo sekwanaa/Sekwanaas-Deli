d852e7e730853616f72adddbfad0d7f67c445c68
sekwanaa <sk123rulz@gmail.com>
2024-06-18 16:31:12 -0400

    Added SpringBoot dependencies

--- src/main/java/com/pluralsight/DeliApp.java
+++ src/main/java/com/pluralsight/DeliApp.java
@@ -2,13 +2,19 @@ package com.pluralsight;
 
 import com.pluralsight.util.Inputs;
 import com.pluralsight.ui.HomeScreen;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class Main {
+@SpringBootApplication
+public class DeliApp {
 
 
     public static void main(String[] args) {
+
         Inputs.openScanner();
 
+        SpringApplication.run(DeliApp.class, args);
+        
         HomeScreen.displayHomeScreen();
 
         Inputs.closeScanner();
